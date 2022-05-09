d827508af0a1c9a2283571c94cf39937877c968a
RafalGromulski <rafalgromulski@wp.pl>
2022-05-09 21:48:56 +0200

    New modifications was added: new way of data loading, concatenation of data

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -1,17 +1,18 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 pd.set_option('display.width', 200)
 pd.set_option('display.max_columns', 15)
 pd.set_option('display.max_rows', 100)
+# pd.set_option('display.max_rows', None)
 
 bedroom_sensor = r"./last_year_room.csv"
 hall_sensor = r"./last_year_hall.csv"
 bathroom_sensor = r"./last_year_bathroom.csv"
 
-bedroom = pd.read_csv(bedroom_sensor, delimiter=";", parse_dates=["DateTime"])
-hall = pd.read_csv(hall_sensor, delimiter=";", parse_dates=["DateTime"])
-bathroom = pd.read_csv(bathroom_sensor, delimiter=";", parse_dates=["DateTime"])
+bedroom = pd.read_csv(bedroom_sensor, delimiter=";", parse_dates=["DateTime"], decimal=",")
+hall = pd.read_csv(hall_sensor, delimiter=";", parse_dates=["DateTime"], decimal=",")
+bathroom = pd.read_csv(bathroom_sensor, delimiter=";", parse_dates=["DateTime"], decimal=",")
 
 bedroom = bedroom.set_index("DateTime")
 hall = hall.set_index("DateTime")
@@ -21,6 +22,14 @@ bedroom.columns = ["Humidity", "Average_temperature", "Min_temperature", "Max_te
 hall.columns = ["Humidity", "Average_temperature", "Min_temperature", "Max_temperature"]
 bathroom.columns = ["Humidity", "Average_temperature", "Min_temperature", "Max_temperature"]
 
-# print(bedroom)
-# print(hall)
-# print(bathroom)
+bedroom = bedroom.round(decimals=1)
+hall = hall.round(decimals=1)
+bathroom = bathroom.round(decimals=1)
+
+x = pd.concat((bedroom.loc["2021-08", "Humidity"],
+               hall.loc["2021-08", "Humidity"],
+               bathroom.loc["2021-08", "Humidity"]
+               ), axis="columns")
+
+print(x)
+
